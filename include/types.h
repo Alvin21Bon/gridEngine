a9e07f5226e22b44cab0c1e72a97942ff0844101
Alvin21Bon <alvin21bon@gmail.com>
2024-07-22 21:26:43 -0400

    initial planned types

--- include/types.h
+++ include/types.h
@@ -1,6 +1,27 @@
 #ifndef GRID_TYPES_H
 #define GRID_TYPES_H
 
+#include "shared.h"
 
+struct CanvasPixel {
+	GLboolean isSolid;
+	Vec3 color;
+};
+
+struct CanvasBorder {
+	GLboolean isVisible;
+	Vec3 color;
+	float thickness;
+};
+
+struct CoordinateCanvas {
+	Vec2 origin;
+	Vec2 size;
+	struct gridUnitCnt {
+		unsigned int x, y;
+	};
+	struct CanvasPixel** canvasData; // dynamically allocated 2D array based on the gridUnitCnt values
+	struct CanvasBorder border;
+};
 
 #endif // GRID_TYPES_H
