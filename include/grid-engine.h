c33f9c652fb6a02f2ba6ab5ebf909323fa41c614
Alvin21Bon <alvin21bon@gmail.com>
2024-07-24 02:34:10 -0400

    added new files

--- include/grid-engine.h
+++ include/grid-engine.h
@@ -1,7 +1,9 @@
 #ifndef GRID_GRID_ENGINE_H
 #define GRID_GRID_ENGINE_H
 
-#include "types.h"
+#include "canvas-border.h"
+#include "canvas-pixel.h"
+#include "coordinate-canvas.h"
 #include "shared.h"
 
 #endif // GRID_GRID_ENGINE_H
