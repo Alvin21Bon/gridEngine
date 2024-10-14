ecc2907da00e5f875119325563261829aa4b5cfd
Alvin21Bon <alvin21bon@gmail.com>
2024-10-14 10:08:04 -0400

    get ready for future refactor where only the input data is stored in the window user pointer

--- src/utility/glfw/input-data.c
+++ src/utility/glfw/input-data.c
@@ -7,25 +7,25 @@ struct InputData inputData()
 	return inputData;
 }
 
-// NOTE: the engine GameState is always stored in the window user pointer
+// NOTE: the engine InputData struct is always stored in the window user pointer
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	struct GameState* game = glfwGetWindowUserPointer(window);
-	game->inputData.keyFlags[key] = action; // press is true, release is false, repeat doesnt matter
+	struct InputData* inputData = glfwGetWindowUserPointer(window);
+	inputData->keyFlags[key] = action; // press is true, release is false, repeat doesnt matter
 }
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 {
-	struct GameState* game = glfwGetWindowUserPointer(window);
-	game->inputData.mouseFlags[button] = action;
+	struct InputData* inputData = glfwGetWindowUserPointer(window);
+	inputData->mouseFlags[button] = action;
 }
 void cursorPosCallback(GLFWwindow* window, double xPos, double yPos)
 {
-	struct GameState* game = glfwGetWindowUserPointer(window);
-	game->inputData.cursorPos.x = xPos;
+	struct InputData* inputData = glfwGetWindowUserPointer(window);
+	inputData->cursorPos.x = xPos;
 
 	// invert the y because by default, the coordinates are relative to the top left corner (so dumb)
 	int height;
 	glfwGetWindowSize(window, NULL, &height);
-	game->inputData.cursorPos.y = height - yPos;
+	inputData->cursorPos.y = height - yPos;
 }
