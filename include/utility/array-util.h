d6af5f5cd077c4ce977fc78653ee3947ef697cfb
Alvin21Bon <alvin21bon@gmail.com>
2024-10-15 09:33:39 -0400

    decided that shift array functions are unecessary

--- include/utility/array-util.h
+++ include/utility/array-util.h
@@ -2,21 +2,10 @@
 
 #include "shared.h"
 
-// NOTE: last element in array is unmodified
-// @returns {bool} false on error
-bool SHIFT_ARRAY_ELEMENTS_LEFT(void* const array, size_t idxToShiftAt, const size_t sizeOfElements, const size_t arrayLength)
+// Takes a splice of memory on range [start, end) and copies it to another address
+// @exception {SegmentationFault}
+static inline void MEMMOVE_RANGE(void* const destination, void* const spliceRangeStart, void* const spliceRangeEnd)
 {
-	if (idxToShiftAt == 0) idxToShiftAt = 1; // seg fault if this is done on idx=0
-	else if (idxToShiftAt >= arrayLength) 
-	{
-		printf("SHIFT_ARRAY_ELEMENTS_LEFT ERROR: shift idx outside of array range\n");
-		return false;
-	}
-
-	char* const arrayFirstAddress = (char*)array;
-	char* const arrayLastAddress = arrayFirstAddress + (sizeOfElements * arrayLength);
-	char* const idxToShiftAtAddress = arrayFirstAddress + (sizeOfElements * idxToShiftAt);
-
-	memmove(idxToShiftAtAddress - sizeOfElements, idxToShiftAtAddress, arrayLastAddress - idxToShiftAtAddress);
-	return true;
+	memmove(destination, spliceRangeStart, (char*)spliceRangeEnd - (char*)spliceRangeStart);
 }
+
