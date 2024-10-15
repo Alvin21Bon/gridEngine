109e853c031529243bdb76361bf0042ead59194d
Alvin21Bon <alvin21bon@gmail.com>
2024-10-15 08:49:19 -0400

    temp for now, but instead define error and success as bools. also ERROR IS ZERO, this is just nicer and can always be handle in main

--- include/engine/grid-engine-states.h
+++ include/engine/grid-engine-states.h
@@ -1,7 +1,7 @@
 #pragma once
 
 enum GRID_ENGINE_ERROR_HANDLING {
-	GRID_ENGINE_SUCCESS = 0,
-	GRID_ENGINE_ERROR = 1 // this is 1 so it can be returned from main
+	GRID_ENGINE_SUCCESSx = 0,
+	GRID_ENGINE_ERRORx = 1 // this is 1 so it can be returned from main
 };
 
