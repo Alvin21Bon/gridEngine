696d3ef11b4e0d05fe9aa2e72ca8de3f8b699819
Alvin21Bon <alvin21bon@gmail.com>
2024-10-15 22:16:31 -0400

    initial temp defintion of GridEngine, my head hurts. lets plan this out more

---
+++ include/engine/grid-engine-new.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../utility/shared.h"
+#include "../game/game-state-new.h"
+
+/*
+ * A singleton instance of this struct is made on initialization of the engine. This struct basically embodies the entire
+ * running session of the engine. APIs to the engine are provided to the user, but generally the user will not 
+ * interact with this struct much.
+ *
+ * The user can work with the gamestate and edit engine configurations
+ * in the user defined engine init function.
+*/
+struct GridEngine {
+	struct GameState1 gameState;
+	struct ShaderProgramManager shaderProgramManager;
+	struct CursorStateManager cursorStateManager;
+	// ENGINE OPTIONS
+	// WINDOW/GLFW
+};
+
+void gridEngineInit(struct GridEngine* const engine);
