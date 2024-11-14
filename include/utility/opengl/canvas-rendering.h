83e9daab998feb5468fc5232a403560b18198322
Alvin21Bon <alvin21bon@gmail.com>
2024-11-13 20:20:42 -0500

    new philosophy with canvas rendering struct that it is not so important to abstractify the coordinatecanvas from it. doesnt make sense. it just needs to effectivedly add onto the functionalirty of the coordinatecanvas to render it onto the scren and deal with any OpenGL stuff

--- include/utility/opengl/canvas-rendering.h
+++ include/utility/opengl/canvas-rendering.h
@@ -9,31 +9,21 @@ struct ShaderProgramManager;
 /*
  * This struct encapsulates any rendering or utilization of opengl from the canvas.
  * Thus, the CoordinateCanvas struct will simply handle any data of the canvas, and
- * this struct handles getting the canvas to render to the screen. 
- *
- * These functionalities work entirely indepdentely of one another. I feel
- * like this makes the functionality limits of every struct very nicely defined. 
- * This allows for rendering data to be modified and tweaked without needing for the
- * entire canvas data to change. 
+ * this struct and functions handles getting the canvas to render to the screen. 
 */
 struct CanvasRenderer {
 	uint VBO, VAO; // EBO is the same across all canvas renderers, thus does not need to be stored here
-	size_t sizeOfVertexData;
-
-	struct {
-		struct Viewport canvasViewport;
-		struct Viewport borderViewport;
-		size_t numOfInstances; // for rendering the pixels
-		bool shouldBeRendered;
-	} settings;
+	size_t sizeOfPixelArray;
+	struct Viewport canvasViewport;
+	struct Viewport borderViewport;
 };
 
-struct CanvasRenderer canvasRenderer(const struct CoordinateCanvas* const canvas);
-void canvasRendererDestory(struct CanvasRenderer* const canvasRenderer);
+struct CanvasRenderer canvasRenderer(struct CoordinateCanvas* const canvas);
+void canvasUpdateRenderer(struct CoordinateCanvas* const canvas);
+void canvasRendererDestroy(struct CanvasRenderer* const canvasRenderer);
 
-void canvasRendererUpdateSettings(struct CanvasRenderer* const canvasRenderer, const struct CoordinateCanvas* const canvas);
 void canvasRendererCreateGLObjects(struct CanvasRenderer* const canvasRenderer);
-void canvasRendererAllocateBuffers(struct CanvasRenderer* const canvasRenderer);
-void canvasRendererFillBuffers(struct CanvasRenderer* const canvasRenderer, const struct CoordinateCanvas* const canvas);
+void canvasAllocateRendererVertexBuffer(struct CoordinateCanvas* const canvas);
+void canvasUpdateRendererVertexBuffer(struct CoordinateCanvas* const canvas);
 
-void canvasRender(const struct CanvasRenderer* const canvasRenderer, struct ShaderProgramManager* const shaderProgramManager);
+void canvasDraw(struct CoordinateCanvas* const canvas, struct ShaderProgramManager* const shaderProgramManager);
--- src/utility/opengl/canvas-rendering.c
+++ src/utility/opengl/canvas-rendering.c
@@ -1,11 +1,11 @@
 #include "utility/opengl/canvas-rendering.h"
 
-struct CanvasRenderer canvasRenderer(const struct CoordinateCanvas* const canvas);
-void canvasRendererDestory(struct CanvasRenderer* const canvasRenderer);
+struct CanvasRenderer canvasRenderer(struct CoordinateCanvas* const canvas);
+void canvasUpdateRenderer(struct CoordinateCanvas* const canvas);
+void canvasRendererDestroy(struct CanvasRenderer* const canvasRenderer);
 
-void canvasRendererUpdateSettings(struct CanvasRenderer* const canvasRenderer, const struct CoordinateCanvas* const canvas);
 void canvasRendererCreateGLObjects(struct CanvasRenderer* const canvasRenderer);
-void canvasRendererAllocateBuffers(struct CanvasRenderer* const canvasRenderer);
-void canvasRendererFillBuffers(struct CanvasRenderer* const canvasRenderer, const struct CoordinateCanvas* const canvas);
+void canvasAllocateRendererVertexBuffer(struct CoordinateCanvas* const canvas);
+void canvasUpdateRendererVertexBuffer(struct CoordinateCanvas* const canvas);
 
-void canvasRender(const struct CanvasRenderer* const canvasRenderer, struct ShaderProgramManager* const shaderProgramManager);
+void canvasDraw(struct CoordinateCanvas* const canvas, struct ShaderProgramManager* const shaderProgramManager);
