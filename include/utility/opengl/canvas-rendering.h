566964de668d4bfeab4c55343465d717076c3098
Alvin21Bon <alvin21bon@gmail.com>
2024-11-14 18:01:58 -0500

    function for updating the renderer viewports

--- include/utility/opengl/canvas-rendering.h
+++ include/utility/opengl/canvas-rendering.h
@@ -23,6 +23,7 @@ void canvasUpdateRenderer(struct CoordinateCanvas* const canvas);
 void canvasRendererDestroy(struct CanvasRenderer* const canvasRenderer);
 
 void canvasRendererCreateGLObjects(struct CanvasRenderer* const canvasRenderer);
+void canvasUpdateRendererViewports(struct CoordinateCanvas* const canvas);
 void canvasAllocateRendererVertexBuffer(struct CoordinateCanvas* const canvas);
 void canvasUpdateRendererVertexBuffer(struct CoordinateCanvas* const canvas);
 
