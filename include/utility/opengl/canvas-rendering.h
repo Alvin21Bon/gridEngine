28b82ce189f26813a4ff33545c298e2a995fcacd
Alvin21Bon <alvin21bon@gmail.com>
2024-11-14 18:14:20 -0500

    realized these should just be static functions in the src file

--- include/utility/opengl/canvas-rendering.h
+++ include/utility/opengl/canvas-rendering.h
@@ -22,9 +22,4 @@ struct CanvasRenderer canvasRenderer(struct CoordinateCanvas* const canvas);
 void canvasUpdateRenderer(struct CoordinateCanvas* const canvas);
 void canvasRendererDestroy(struct CanvasRenderer* const canvasRenderer);
 
-void canvasRendererCreateGLObjects(struct CanvasRenderer* const canvasRenderer);
-void canvasUpdateRendererViewports(struct CoordinateCanvas* const canvas);
-void canvasAllocateRendererVertexBuffer(struct CoordinateCanvas* const canvas);
-void canvasUpdateRendererVertexBuffer(struct CoordinateCanvas* const canvas);
-
 void canvasDraw(struct CoordinateCanvas* const canvas, struct ShaderProgramManager* const shaderProgramManager);
