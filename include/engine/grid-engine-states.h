4838315f923fd373fd30e34c6d8c689ead34874e
Alvin21Bon <alvin21bon@gmail.com>
2024-11-20 17:37:07 -0500

    more info on grid engine states

--- include/engine/grid-engine-states.h
+++ include/engine/grid-engine-states.h
@@ -1,7 +1,11 @@
 #pragma once
 
+// These values are returned from the GameState pre/post update functions, and any of the
+// user defined GameObject update functions. 
+// GRID_ENGINE_SUCCESS and GRID_ENGINE_ERROR signal to cease the game loop on a success or failure condition
+// GRID_ENGINE_RUNNING dicates normal game behavior, and signals to progress with the game loop as normal
+// GRID_ENGINE_PAUSED signals a pause, which will hault game updates until the state is changed
 enum GridEngineStates {
-	// IF THE ENGINE IS IN ONE OF THESE TWO STATES, PROGRAM WILL IMMEDIATELY TERMINATE AND RETURN THE ENGINE STATE FROM MAIN
 	GRID_ENGINE_SUCCESS, 
 	GRID_ENGINE_ERROR, 
 
