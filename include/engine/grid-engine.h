3e8b0ed104e5c92ba5c4b3a2a753106ae1bb3c2c
Alvin21Bon <alvin21bon@gmail.com>
2024-11-21 02:45:58 -0500

    changes to grid engine struct to account for new pausing functionality

--- include/engine/grid-engine.h
+++ include/engine/grid-engine.h
@@ -3,11 +3,12 @@
 #include "game/game-state.h"
 #include "utility/opengl/shader-programs.h"
 #include "utility/glfw/cursor-state-manager.h"
+#include "engine/grid-engine-states.h"
 
 /*
  * A singleton instance of this struct is made on initialization of the engine. This struct basically embodies the entire
- * running session of the engine. APIs to the engine are provided to the user, but generally the user will not 
- * interact with this struct much.
+ * running session of the engine. APIs to the engine are provided to the user, since none of the GridEngine
+ * struct definition is not visible to the user
  *
  * The user can work with the gamestate and edit engine configurations
  * in the user defined engine init function.
@@ -16,8 +17,16 @@ struct GridEngine {
 	struct GameState gameState;
 	struct ShaderProgramManager shaderProgramManager;
 	struct CursorStateManager cursorStateManager;
-	// ENGINE OPTIONS
+
+	enum GridEngineStates state;
+	enum GridEngineStates (*pausedStateFunction)(struct GameState* const);
+
 	// WINDOW/GLFW
 };
 
+// user defined init function
 void gridEngineInit(struct GridEngine* const engine);
+
+// getters and setters
+void gridEngineAttachPausedStateFunction(struct GridEngine* const engine, enum GridEngineStates (*pausedStateFunction)(struct GameState* const));
+struct GameState* gridEngineGetGameState(const struct GridEngine* const engine);
