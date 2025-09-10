649eee2e0fb567b8c5e33c3edaef59df3ab63d5a
Alvin21Bon <alvin21bon@gmail.com>
2025-09-10 14:00:03 -0400

    stores engine input data in glfw window user pointer for callbacks

--- src/utility/glfw/window.c
+++ src/utility/glfw/window.c
@@ -45,6 +45,8 @@ struct GridWindow gridWindow()
 	gridWindow.input = inputData();
 	gridWindow.cursorShapeManager = cursorShapeManager(window);
 
+	glfwSetWindowUserPointer(window, &gridWindow.input); // remains in window user pointer for input callbacks
+
 	return gridWindow;
 }
 void gridWindowUpdate(struct GridWindow* const window)
