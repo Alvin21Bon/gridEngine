07a0c7743051d29e29e7ca213e016a0eea427297
Alvin21Bon <alvin21bon@gmail.com>
2025-09-12 00:06:26 -0400

    added option to toggle built in cursor cavnas interactions

--- include/engine/grid-engine-options.h
+++ include/engine/grid-engine-options.h
@@ -22,6 +22,8 @@ enum GridEngineOptions {
 	GRID_OPTION_LOGGING_DISABLE_FILEPATH_EXISTS_WARNING,
 	// VALUE TYPE: char*
 	GRID_OPTION_LOGGING_FILEPATH,
+	// VALUE TYPE: bool
+	GRID_OPTION_BUILT_IN_ENABLE_CURSOR_CANVAS_INTERACTIONS, // TODO: make changing this option work during engine run-time somehow
 };
 
 extern uint GRID_MAX_FPS;
@@ -32,5 +34,6 @@ extern uVec2 GRID_WINDOW_SIZE;
 extern enum LoggingLevels GRID_LOGGING_LEVEL;
 extern bool GRID_LOGGING_DISABLE_FILEPATH_EXISTS_WARNING;
 extern char* GRID_LOGGING_FILEPATH;
+extern bool GRID_BUILT_IN_ENABLE_CURSOR_CANVAS_INTERACTIONS;
 
 void gridEngineChangeOption(const enum GridEngineOptions option, const void* const optionValue);
--- src/engine/grid-engine-options.c
+++ src/engine/grid-engine-options.c
@@ -19,6 +19,7 @@ uVec2 GRID_WINDOW_SIZE = {.width = 1000, .height = 1000};
 enum LoggingLevels GRID_LOGGING_LEVEL = GRID_LOGGING_WARN;
 bool GRID_LOGGING_DISABLE_FILEPATH_EXISTS_WARNING = false;
 char* GRID_LOGGING_FILEPATH = "";
+bool GRID_BUILT_IN_ENABLE_CURSOR_CANVAS_INTERACTIONS = true;
 
 void gridEngineChangeOption(const enum GridEngineOptions option, const void* const optionValue)
 {
@@ -84,6 +85,9 @@ void gridEngineChangeOption(const enum GridEngineOptions option, const void* con
 			}
 
 			GRID_LOGGING_FILEPATH = castedLoggingFilepath;
+			break;
+		case GRID_OPTION_BUILT_IN_ENABLE_CURSOR_CANVAS_INTERACTIONS:
+			GRID_BUILT_IN_ENABLE_CURSOR_CANVAS_INTERACTIONS = *(bool*)optionValue;
 			break;
 		default:
 			LOG(GRID_LOGGING_WARN, __func__, __LINE__, "option not of enum GridEngineOptions\n");
